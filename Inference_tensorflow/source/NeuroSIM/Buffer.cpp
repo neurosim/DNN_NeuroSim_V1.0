71a4fb05932c16c9f683f2e3000432007bc719fb
neurosim <ysm1030@gmail.com>
2019-10-09 16:54:14 -0400

    Update Buffer.cpp

--- Inference_tensorflow/source/NeuroSIM/Buffer.cpp
+++ Inference_tensorflow/source/NeuroSIM/Buffer.cpp
@@ -170,8 +170,8 @@ void Buffer::CalculateLatency(double numAccessBitRead, double numRead, double nu
 			readWholeLatency += ((double) 1/clkFreq/2)*((double) numBit/interface_width);  // assume dff need half clock cycle to access
 			writeWholeLatency += wlDecoder.writeLatency + ((double) 1/clkFreq/2)*((double) numBit/interface_width);
 		}
-		avgBitReadLatency = (double) readWholeLatency/numBit;     // average latency per bit(sec/bit)
-		avgBitWriteLatency = (double) writeWholeLatency/numBit;
+		avgBitReadLatency = (double) readWholeLatency/(numBit/interface_width);     // average latency per line(sec/line)
+		avgBitWriteLatency = (double) writeWholeLatency/(numBit/interface_width);
 		readLatency = avgBitReadLatency*numRead;
 		writeLatency = avgBitWriteLatency*numWrite;
 	}
