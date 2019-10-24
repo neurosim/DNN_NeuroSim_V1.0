52b128fe922322d3f3c6d3f4706bb3f4e7e59d38
neurosim <ysm1030@gmail.com>
2019-10-24 12:51:13 -0400

    Add files via upload

--- Inference_tensorflow/source/NeuroSIM/SubArray.cpp
+++ Inference_tensorflow/source/NeuroSIM/SubArray.cpp
@@ -182,10 +182,12 @@ void SubArray::Initialize(int _numRow, int _numCol, double _unitWireRes){  //ini
     } else if (cell.memCellType == Type::RRAM || cell.memCellType == Type::FeFET) {
 		if (cell.accessType == CMOS_access) {	// 1T1R
 			cell.resCellAccess = cell.resistanceOn * IR_DROP_TOLERANCE;    //calculate access CMOS resistance
-			cell.widthAccessCMOS = CalculateOnResistance(tech.featureSize, NMOS, inputParameter.temperature, tech) / cell.resCellAccess;   //get access CMOS width
+			cell.widthAccessCMOS = CalculateOnResistance(tech.featureSize, NMOS, inputParameter.temperature, tech) * IR_DROP_TOLERANCE / cell.resCellAccess;   //get access CMOS width
 			if (cell.widthAccessCMOS > cell.widthInFeatureSize) {	// Place transistor vertically
 				printf("Transistor width of 1T1R=%.2fF is larger than the assigned cell width=%.2fF in layout\n", cell.widthAccessCMOS, cell.widthInFeatureSize);
 				exit(-1);
+			} else {
+				cell.widthAccessCMOS = MAX(cell.widthAccessCMOS, cell.widthInFeatureSize);
 			}
 
 			cell.resMemCellOn = cell.resCellAccess + cell.resistanceOn;        //calculate single memory cell resistance_ON
@@ -210,7 +212,7 @@ void SubArray::Initialize(int _numRow, int _numCol, double _unitWireRes){  //ini
 			double capBL = lengthCol * 0.2e-15/1e-6;
 			int numAdder = (int)ceil(numCol/numColMuxed);   // numCol is divisible by numCellPerSynapse
 			int numInput = numAdder;        //XXX input number of MUX, 
-			double resTg = cell.resMemCellOn * IR_DROP_TOLERANCE;     //transmission gate resistance
+			double resTg = cell.resMemCellOn;     //transmission gate resistance
 			int adderBit = (int)ceil(log2(numRow)) + avgWeightBit;  
 			
 			wlDecoder.Initialize(REGULAR_ROW, (int)ceil(log2(numRow)), false, false);          
@@ -234,7 +236,7 @@ void SubArray::Initialize(int _numRow, int _numCol, double _unitWireRes){  //ini
 				shiftAdd.Initialize(numAdder, adderBit+numReadPulse+1, clkFreq, spikingMode, numReadPulse);
 			}
 		} else if (conventionalParallel) { 
-			double resTg = cell.resMemCellOn / numRow * IR_DROP_TOLERANCE;
+			double resTg = cell.resMemCellOn / numRow;
 			
 			if (cell.accessType == CMOS_access) {
 				wlNewSwitchMatrix.Initialize(numRow, activityRowRead, clkFreq);         
@@ -251,7 +253,7 @@ void SubArray::Initialize(int _numRow, int _numCol, double _unitWireRes){  //ini
 				shiftAdd.Initialize(ceil(numCol/numColMuxed), log2(levelOutput)+numReadPulse+1, clkFreq, spikingMode, numReadPulse);
 			}
 		} else if (BNNsequentialMode || XNORsequentialMode) {       
-			double resTg = cell.resMemCellOn * IR_DROP_TOLERANCE;
+			double resTg = cell.resMemCellOn;
 			int numAdder = (int)ceil(numCol/numColMuxed);  
 			int numInput = numAdder;        
 			int adderBit = (int)ceil(log2(numRow)) + 1; 
@@ -270,7 +272,7 @@ void SubArray::Initialize(int _numRow, int _numCol, double _unitWireRes){  //ini
 			dff.Initialize((adderBit+1)*numAdder, clkFreq); 
 			adder.Initialize(adderBit, numAdder);
 		} else if (BNNparallelMode || XNORparallelMode) {      
-			double resTg = cell.resMemCellOn / numRow * IR_DROP_TOLERANCE;
+			double resTg = cell.resMemCellOn / numRow;
 			
 			if (cell.accessType == CMOS_access) {
 				wlNewSwitchMatrix.Initialize(numRow, activityRowRead, clkFreq);         
@@ -284,7 +286,7 @@ void SubArray::Initialize(int _numRow, int _numCol, double _unitWireRes){  //ini
 			multilevelSenseAmp.Initialize(numCol/numColMuxed, levelOutput, clkFreq, numReadCellPerOperationNeuro, true);
 			multilevelSAEncoder.Initialize(levelOutput, numCol/numColMuxed);
 		} else {
-			double resTg = cell.resMemCellOn / numRow * IR_DROP_TOLERANCE;
+			double resTg = cell.resMemCellOn / numRow;
 			
 			if (cell.accessType == CMOS_access) {
 				wlNewSwitchMatrix.Initialize(numRow, activityRowRead, clkFreq);         
